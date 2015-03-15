f0ef0b5a8bfdac4b0461296715af73213ba8f712
Daniel Stanczak <daniel_stanczak@hotmail.com>
2015-03-15 20:24:27 +0100

    Zawartosc klasy OfferItem.java podzielilem pomiedzy nowo utworzone klasy tj. Money,ProductSnapshot oraz Discount. Teraz bede dostosowywac ich zawartosci i wzajemne relacje

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/Money.java
@@ -1,5 +1,17 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
+import java.math.BigDecimal;
+
 public class Money {
+
+	private BigDecimal totalCost;
+
+	private String currency;
+
+	private BigDecimal value;
+	
+	public Money(BigDecimal value){
+		this.setValue(value);
+	}
 
 }
--- src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/ProductSnapshot.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/offer/ProductSnapshot.java
@@ -1,5 +1,96 @@
 package pl.com.bottega.ecommerce.sales.domain.offer;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class ProductSnapshot {
 
+	// product
+
+	private BigDecimal productPrice;
+
+	private String productId;
+
+	private String productName;
+
+	private Date productSnapshotDate;
+
+	private String productType;
+
+	public ProductSnapshot(BigDecimal productPrice, String productId,
+			String productName, Date productSnapshotDate, String productType) {
+		super();
+		this.productPrice = productPrice;
+		this.productId = productId;
+		this.productName = productName;
+		this.productSnapshotDate = productSnapshotDate;
+		this.productType = productType;
+	}
+
+	public String getProductId() {
+		return productId;
+	}
+
+	public BigDecimal getProductPrice() {
+		return productPrice;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public Date getProductSnapshotDate() {
+		return productSnapshotDate;
+	}
+
+	public String getProductType() {
+		return productType;
+	}
+
+
+	
+	
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			OfferItem other = (OfferItem) obj;
+			if (discount == null) {
+				if (other.discount != null)
+					return false;
+			} else if (!discount.equals(other.discount))
+				return false;
+			if (productName == null) {
+				if (other.productName != null)
+					return false;
+			} else if (!productName.equals(other.productName))
+				return false;
+			if (productPrice == null) {
+				if (other.productPrice != null)
+					return false;
+			} else if (!productPrice.equals(other.productPrice))
+				return false;
+			if (productId == null) {
+				if (other.productId != null)
+					return false;
+			} else if (!productId.equals(other.productId))
+				return false;
+			if (productType != other.productType)
+				return false;
+			if (quantity != other.quantity)
+				return false;
+			if (totalCost == null) {
+				if (other.totalCost != null)
+					return false;
+			} else if (!totalCost.equals(other.totalCost))
+				return false;
+			return true;
+		}
+		
+	}
 }
